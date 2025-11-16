988baa90ec51de22cc2f7e93aed1b1741e51c369
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-11-16 23:57:53 +0100

    1

--- data/casinos.ts
+++ data/casinos.ts
@@ -4,6 +4,7 @@ export interface Casino {
   logo: string
   rating: number
   bonus: string
+  dopBonus?: string
   features: string[]
   isTopChoice?: boolean
   url: string
@@ -29,6 +30,56 @@ export const casinos: Casino[] = [
     features: ["Casino Móvel", "Jogos Exclusivos", "Apostas Desportivas"],
     url: "https://qualityboost.top/pqYpQQJ7",
   },
+      {
+    rank: 3,
+    name: "Boomerang",
+    logo: "/boomerang-bet.svg",
+    rating: 9.5,
+    bonus: "100% até €500 + 200 Giros Grátis",
+    dopBonus: "",
+    features: ["Tjedni Cashback", "Besplatne Vrtnje", "Brza isplata"],
+    url: "https://qualityboost.top/4cPkdZPR",
+  },
+  {
+    rank: 4,
+    name: "BillyBets",
+    logo: "/billybets.webp",
+    rating: 9.4,
+    bonus: "100% até €500 + 200 Giros Grátis",
+    dopBonus: "",
+    features: ["Reload bonus", "Besplatne Vrtnje", "Brza isplata"],
+    url: "https://qualityboost.top/93mKYgt5",
+  },
+  {
+    rank: 6,
+    name: "MrPacho",
+    logo: "/mrpacho.svg",
+    rating: 9.2,
+    bonus: "100% até €500 + 200 Giros Grátis",
+    dopBonus: "",
+    features: ["Besplatne Vrtnje", "Brza isplata"],
+    url: "https://qualityboost.top/3VCrr6mB",
+  },
+  {
+    rank: 7,
+    name: "Winrolla",
+    logo: "/winrolla.webp",
+    rating: 9.0,
+    bonus: "300% até €8000 + 300 GG + 1 Bonus Crab",
+    dopBonus: "",
+    features: ["Besplatne Vrtnje", "Brza isplata"],
+    url: "https://qualityboost.top/4cPkdZPR",
+  },
+  {
+    rank: 8,
+    name: "betibet",
+    logo: "/betibet.svg",
+    rating: 8.9,
+    bonus: "100% até €200 + 50 Giros Grátis",
+    dopBonus: "",
+    features: ["Besplatne Vrtnje", "Brza isplata"],
+    url: "https://qualityboost.top/4cPkdZPR",
+  }
 ]
 
 // Get top casino (rank 1)
